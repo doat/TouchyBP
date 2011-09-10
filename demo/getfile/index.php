03247b8781cc9225ecbafb36008c3b155c0bf6c0
Ran Ben Aharon <ran@ranbena.com>
2011-09-10 17:32:44 +0300

    added new getfile demo to illustrate TouchyPHP::getFile

---
+++ demo/getfile/index.php
@@ -0,0 +1,23 @@
+<?
+    require "../../php/TouchyPHP.php";
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TouchyPHP demo</title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        
+        <? echo TouchyPHP::getFile("css/style.css", TRUE); ?>
+    </head>
+    <body>
+        <div class="image"></div>
+        <br>
+        <img src="<? echo TouchyPHP::getFile("img/image.png", TRUE); ?>" alt="image" />
+        <br>
+        <img src="<? echo TouchyPHP::getFile("img/image.png", FALSE); ?>" alt="image" />
+        <? echo TouchyPHP::getFile("js/scripts.js", TRUE); ?>
+    </body>
+</html>
