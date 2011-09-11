38bcd78fb5eb8994f7c504c9fe8e3c061989d6ed
Ran Ben Aharon <ran@ranbena.com>
2011-09-11 07:42:37 +0300

    changed Indicator setting

--- demo/nav/js/scripts.js
+++ demo/nav/js/scripts.js
@@ -68,7 +68,7 @@ function search(q){
     $page.find("ul").remove();
     var $ul = $("<ul>");
     $page.append($ul);
-    TouchyJS.Nav.Indicator.show({color: '#cccccc'});
+    TouchyJS.Nav.Indicator.show();
     
     // get search results
     $.getJSON('data/search.js?v=2&q='+q, function(response){
